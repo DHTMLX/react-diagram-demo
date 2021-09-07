0a01f943d51b26c4f885385df5f908e965af1a92
Sorokin-Oleg <sorokin.world@gmail.com>
2021-09-07 12:48:08 +0300

    Updates

---
+++ precache-manifest.ced596c93ae7752d6de9f7bd44fcaf34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8807c4bec3e759c9884aa0ab618246c",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.fe77f79bdaa86ae76a64.bundle.js"
+  },
+  {
+    "url": "runtime~main.fe77f79bdaa86ae76a64.bundle.js"
+  },
+  {
+    "revision": "c6f79ac654234654f164",
+    "url": "static/css/main.0aa96643.chunk.css"
+  },
+  {
+    "url": "vendors~main.fe77f79bdaa86ae76a64.bundle.js"
+  },
+  {
+    "url": "vendors~main.fe77f79bdaa86ae76a64.bundle.js.LICENSE.txt"
+  }
+]);
