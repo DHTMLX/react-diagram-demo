2b9b3c331b4871a5eb5fda3bd34061d8b056e971
Ivan Izobov <izobov9@gmail.com>
2024-07-02 03:54:27 +0300

    fix body margin

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import DiagramComponent from "./DiagramComponent";
 import { getData } from "./data";
 import { useState } from "react";
+import "./app.css"
 
 function App() {
   let [data] = useState(getData());
